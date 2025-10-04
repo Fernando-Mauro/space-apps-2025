11b8b509d8dc720f800a707e2cb2406732159043
Fernando Mauro <fermaurolf@gmail.com>
2025-10-04 15:48:19 -0500

    Added some features

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect, useState } from 'react';
 import osmtogeojson from 'osmtogeojson';
@@ -26,12 +26,37 @@ const MapDisplay = () => {
   }, []);
 
   return (
-    <MapContainer center={[17.1182868, -96.4575874]} zoom={14.37} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {geojsonData && <GeoJSON data={geojsonData} />}
+    <MapContainer center={[17.1182868, -96.4575874]} maxBounds={[[17.1160569, -96.4697168], [17.1232868, -96.4525874]]} minZoom={15} zoom={15} style={{ height: '100vh', width: '100%' }}>
+      <LayersControl position="topright">
+        {/* --- CAPAS BASE --- */}
+        <LayersControl.BaseLayer checked name="🗺️ Calles">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="🛰️ Satélite">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; <a href="https://www.esrifrance.fr/">Esri France</a>'
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="🌃 Oscuro">
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* --- CAPAS SUPERPUESTAS (OVERLAYS) --- */}
+        {/* {geojsonData && (
+          <LayersControl.Overlay checked name="📍 Datos OSM">
+            <GeoJSON data={geojsonData} />
+          </LayersControl.Overlay>
+        )} */}
+      </LayersControl>
     </MapContainer>
   );
 };
