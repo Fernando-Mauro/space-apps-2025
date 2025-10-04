4886135196f2588a2c6c38315f30c31293e77705
Fernando Mauro <fermaurolf@gmail.com>
2025-10-04 14:38:41 -0500

    Centered the map and changed a bad image

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -21,7 +21,7 @@ const Map = () => {
   }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[17.1182868,-96.4575874]} zoom={14.37} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
